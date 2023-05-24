803f5259d331d9209463a793fbd54e36a90a3c6d
Limon Islam <72975868+Limon714@users.noreply.github.com>
2023-05-24 14:07:05 +0600

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php 
+
+include "connect.php";
+
+if(isset($_GET['deleteid'])){
+    $id = $_GET['deleteid'];
+}
+
+$sql = "delete from `user` where ID = $id" ;
+
+$result = mysqli_query($con, $sql);
+
+if($result){
+    header('location:display.php');
+}else{
+    die( mysqli_error($con));
+}
+
+?>
